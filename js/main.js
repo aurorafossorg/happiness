00f6be71b17f777c1c5d8d785832a2743f6a9966
Luís Ferreira <lsferreira169@gmail.com>
2018-12-23 20:38:24 +0000

    render quotes to index page
    Signed-off-by: Luís Ferreira <lsferreira169@gmail.com>

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$.ajax({
+	url: ("res/values/" + (navigator.language || navigator.userLanguage) + "/quotes.xml"),
+	success: function (result) {
+		var quotes = result.getElementsByTagName("quotes")[0];
+		$("#quote").append(quotes.children[Math.floor((Math.random() * quotes.childElementCount))].innerHTML);
+	}
+});
