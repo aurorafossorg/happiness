604af3359cade84ab7d47747665e7c3494aebc32
Luís Ferreira <contact@lsferreira.net>
2019-05-12 22:16:53 +0100

    quotes: generalize pages
    Signed-off-by: Luís Ferreira <contact@lsferreira.net>

--- js/quotes.js
+++ js/quotes.js
@@ -1,5 +1,5 @@
 $.ajax({
-	url: ("res/values/" + (navigator.language || navigator.userLanguage) + "/quotes.xml"),
+	url: ("/res/values/" + (navigator.language || navigator.userLanguage) + "/quotes.xml"),
 	success: function (result) {
 		var quotes = result.getElementsByTagName("quotes")[0];
 		$("#quote").append(quotes.children[Math.floor((Math.random() * quotes.childElementCount))].innerHTML);
---
+++ quotes.md
@@ -0,0 +1,4 @@
+---
+layout: quotes
+title: Quotes
+---
